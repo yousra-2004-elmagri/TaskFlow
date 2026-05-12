ff675c92b5b3cd574612dbd89a6c6cc7aeb19846
yousra-2004-elmagri <yousra.elmagri@etu.uae.ac.ma>
2026-05-12 16:32:39 +0100

    feat: add authentification middleware

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Task = require("../models/Task");
-const auth = require("../middleware/auth");
+const auth = require("../middleware/authMiddleware");
 
 // إنشاء مهمة
 router.post("/", auth, async (req, res) => {
